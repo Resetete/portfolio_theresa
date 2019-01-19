0382030a10bae9788cfb248f93437d6097ed3b71
Theresa <tmannschatz@gmail.com>
2019-01-19 14:38:25 +0100

    rewritten animation code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
 var count = 0;
 
+// moves the chameleon image from top to the bottom chamaeleon
+//$("#chamaeleon").click(function(){
+$("#chamaeleon").hover(function(){
 
-if (count<3) {
-    $("#chamaeleon").hover(function(){
-        $("#chamaeleon-fig").animate( { left: '+40vw' }, "slow", "swing" );
-        console.log("in loop"+count);
+    if (count==0)
+      {
+        $("#chamaeleon").animate( { left: '+35vw' }, "slow", "swing" );
         count++;
-        if (count<11) {
-          console.log("do some movement");
-          $("#chamaeleon-fig").animate( { top: '+=60vh' }, "slow", "swing" );
-        } else {console.log("stop")
-                $("#chamaeleon-fig").animate( { left:'-=40vw'}, "slow", "swing",function(){$("#chamaeleon-fig").hide()} );
-        };
-    });
-}
+        console.log("go right, " + "count: "+count);
+      }
+
+    else if (count==1 | count<11)
+      {
+        $("#chamaeleon").animate( { top: '+=460px' }, "slow", "swing" );
+        count++;
+        console.log("go down, " + "count: " + count);
+      }
+
+    else if (count==11)
+      {
+        $("#chamaeleon").animate( { left:'-=40vw'},"slow","swing",function(){$("#chamaeleon-fig").hide(100)} );
+        console.log("go left and stop, " + "count: " + count);
+      }
+
+    else
+        {
+        console.log("should finaly stop count >11");
+        }
+});
+
+
+
 });
