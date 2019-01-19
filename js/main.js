da56ac51cd5bb6cfad0feb468da486f20b38c721
Theresa <tmannschatz@gmail.com>
2019-01-19 13:53:26 +0100

    add hide() as complete animation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var count = 0;
 
 
 if (count<3) {
-    $("#chamaeleon").click(function(){
+    $("#chamaeleon").hover(function(){
         $("#chamaeleon-fig").animate( { left: '+40vw' }, "slow", "swing" );
         console.log("in loop"+count);
         count++;
@@ -11,8 +11,7 @@ if (count<3) {
           console.log("do some movement");
           $("#chamaeleon-fig").animate( { top: '+=60vh' }, "slow", "swing" );
         } else {console.log("stop")
-                $("#chamaeleon-fig").stop();
-                $("#chamaeleon-fig").hide()
+                $("#chamaeleon-fig").animate( { left:'-=40vw'}, "slow", "swing",function(){$("#chamaeleon-fig").hide()} );
         };
     });
 }
